9dd1330655b637b2add1a13ac89b88781370e828
JeremdY <jeremy.bourdillat.09@gmail.com>
2024-04-07 01:40:23 +0200

    Implemented tree structure + add nodes

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,69 @@
 use pyo3::prelude::*;
+use pyo3::exceptions::*;
+use pyo3::types::PyList;
+//use pyo3::types::{PyAny, Bound};
 
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+
+#[pyclass]
+struct Tree {
+
+    #[pyo3(get, name = "children")]
+    _children: Vec<Vec<usize>>, 
+
+    #[pyo3(get, name = "transitions")]
+    _transitions: Vec<Vec<Py<PyAny>>>,
+
+    #[pyo3(get, name = "parents")]
+    _parents: Vec<Option<usize>>,
+
+    #[pyo3(get, name = "values")]
+    _values: Vec<Option<Py<PyAny>>>
+}
+
+#[pymethods]
+impl Tree {
+
+    #[new]
+    fn new() -> Self {
+        Tree{
+            _children: Vec::<Vec<usize>>::new(),
+            _transitions: Vec::<Vec<Py<PyAny>>>::new(),
+            _parents: Vec::<Option<usize>>::new(),
+            _values: Vec::<Option<Py<PyAny>>>::new()
+        }
+    }
+
+    #[pyo3(signature=(parent, conns, trans, value, parent_transition))]
+    ///  Function to add a node to the tree.
+    fn _add_node(&mut self, parent: Option<usize>, conns: Vec<usize>, trans: Vec<Py<PyAny>>, value: &Bound<'_, PyAny>, parent_transition: &Bound<'_, PyAny>) -> PyResult<usize> {
+
+        let index: usize = self._children.len();
+
+        // If a parent was given, add the new node to the children list of the parent
+        if let Some(p) = parent {
+
+            if let (Some(ch), Some(tr)) = (self._children.get_mut(p), self._transitions.get_mut(p)) {
+                ch.push(index);
+                tr.push(parent_transition.clone().unbind());
+            }
+            else {
+                return Err(PyIndexError::new_err("Parent should point to a valid node!"));
+            }
+        }
+
+        self._children.push(conns);
+        self._transitions.push(trans);
+        self._parents.push(parent);
+        self._values.push(Some(value.clone().unbind()));
+
+        Ok(index+1)
+    }
 }
 
 /// A Python module implemented in Rust.
 #[pymodule]
-fn ramage(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+fn ramage(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    //m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_class::<Tree>()?;
     Ok(())
 }
